46436b9946d217c0479bdc87a2b0fefce63d053f
Piepench Fall Desteur <shmurdanoah@gmail.com>
2025-01-21 11:29:22 +0100

    alimentation component structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { FabricationComponent } from './fabrication/fabrication.component';
 import { HomeComponent } from './home/home.component';
+import { AlimentationComponent } from './alimentation/alimentation.component';
 
 export const routes: Routes = [
   {
@@ -8,4 +9,5 @@ export const routes: Routes = [
     component: HomeComponent,
   },
   { path: 'fabrication', component: FabricationComponent },
+  { path: 'alimentation', component: AlimentationComponent },
 ];
