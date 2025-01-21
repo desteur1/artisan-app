71c83d766d7736e15a4628541b6fd2ce5d62ddff
Piepench Fall Desteur <shmurdanoah@gmail.com>
2025-01-21 12:17:08 +0100

    batiment component structure

---
+++ src/app/batiment/batiment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-batiment',
+  imports: [],
+  templateUrl: './batiment.component.html',
+  styleUrl: './batiment.component.scss',
+})
+export class BatimentComponent {
+  title = 'Artisans Bâtiment';
+}
